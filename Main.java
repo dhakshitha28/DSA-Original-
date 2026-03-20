c982c4a7b98ddd21763ab27cdb3e6062ef6a2fbc
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-03-20 19:36:47 +0530

    count

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Solution {
+    public int countDigits(int n) {
+        int countDigit = 0;
+
+        String value = String.valueOf(n);
+        char ch[] = value.toCharArray();
+
+        for(int i = 0; i < ch.length; i++){
+            if(ch[0] == '0'){
+                break;
+            }
+            else {
+                countDigit++;
+            }
+        }
+
+        return countDigit;
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Solution solution=new Solution();
+        int n= sc.nextInt();
+        System.out.println(solution.countDigits(n));
+    }
+}
