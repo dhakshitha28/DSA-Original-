2c9f2bb4c8eb1552f9328f7b9e65d60e7229333c
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-04-02 19:49:34 +0530

    Added ArmstrongNumbers file

---
+++ ANumber/ArmstrongNumbers.java
@@ -0,0 +1,20 @@
+package ANumber;
+
+import java.util.List;
+
+class Solution {
+    static boolean armstrongNumber(int n) {
+        boolean result =false;
+         String num=String.valueOf(n);
+         char[] values=num.toCharArray();
+         int sum=0;
+         for(int i=0;i<values.length;i++){
+
+         }
+         return result;
+    }
+         }
+
+public class ArmstrongNumbers {
+
+}
