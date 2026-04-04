80e504b998330005f33a7b9efbdbe177f27b28a3
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-04-04 21:22:19 +0530

    Armstrong Number2

--- ANumber/ArmstrongNumbers.java
+++ ANumber/ArmstrongNumbers.java
@@ -1,20 +1,36 @@
 package ANumber;
 
-import java.util.List;
+import java.util.Scanner;
 
 class Solution {
     static boolean armstrongNumber(int n) {
-        boolean result =false;
-         String num=String.valueOf(n);
-         char[] values=num.toCharArray();
-         int sum=0;
-         for(int i=0;i<values.length;i++){
+        int duplicate = n;
+        int sum = 0;
 
-         }
-         return result;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            sum = sum + (lastDigit * lastDigit * lastDigit);
+            n = n / 10;
+        }
+
+        return duplicate == sum;
     }
-         }
+}
 
 public class ArmstrongNumbers {
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (Solution.armstrongNumber(number)) {
+            System.out.println(number + " is an Armstrong number");
+        } else {
+            System.out.println(number + " is NOT an Armstrong number");
+        }
+
+        sc.close();
+    }
 }
