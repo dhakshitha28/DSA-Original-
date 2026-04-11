53cc9cf71bd8f74f43924b46525999c64e05fd0b
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-04-11 23:29:49 +0530

    Leaft Rotate Array by K place

---
+++ LeftRotateArrayBy_K_Place.java
@@ -0,0 +1,27 @@
+public class LeftRotateArrayBy_K_Place {
+    class Solution {
+        public void rotate(int[] nums, int k) {
+
+            int n = nums.length;
+            int[] temp = new int[n];
+
+            k = k % n;
+
+            int count = 0;
+
+            for(int i = n - k; i < n; i++){
+                temp[count++] = nums[i];
+            }
+
+
+            for(int i = 0; i < n - k; i++){
+                temp[count++] = nums[i];
+            }
+
+
+            for(int i = 0; i < n; i++){
+                nums[i] = temp[i];
+            }
+        }
+    }
+}
