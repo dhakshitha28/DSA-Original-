7ef4b62d1d1452d71c8f9dfd0f84fee217e9d2b4
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-04-11 23:27:13 +0530

    Roate String

---
+++ RoateString.java
@@ -0,0 +1,14 @@
+public class RoateString {
+    public boolean rotateString(String s, String goal) {
+        int original=s.length();
+        int a=goal.length();
+        if(original!=a){
+            return false;
+        }
+
+        s=s+s;
+        boolean result=s.contains(goal);
+        return result;
+    }
+}
+}
