c4ddcdc221f9433d52aa1bf2cc475217983a5d4e
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-04-13 15:41:06 +0530

    reverse String

---
+++ ReverseString.java
@@ -0,0 +1,17 @@
+public class ReverseString {
+        public void reverseString(char[] s) {
+
+            char [] a=new char[s.length];
+            int j=0;
+
+            for(int i=s.length-1;i>=0;i--){
+                a[j]=s[i];
+                j++;
+            }
+
+            for(int i=0;i<s.length;i++){
+                s[i]=a[i];
+            }
+        }
+}
+
